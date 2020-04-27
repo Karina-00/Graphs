505cf37238dc18139f2f71ce9737a431b9874bf6
Karina-00 <karina1szubert@gmail.com>
2020-04-27 12:43:08 +0200

    wyswietlanie macierzy sasiedztwa, listy nastepnikow i tabeli krawedzi

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from tabulate import tabulate
+from copy import deepcopy
+import itertools
+
+
 def generate():
     """Generuje graf"""
     pass
@@ -6,26 +11,49 @@ def generate():
 
 def create_graph():
     """Tworzy graf"""
-    pass
-    # return graph
+    v = int(input("Podaj ilosc wierzcholkow grafu:"))
+    print("Podaj kolejne wiersze macierzy sasiedztwa:")
+    matrix = []
+    for i in range(v):
+        line = list(map(int, input(f"{i+1} linia:").split()))
+        matrix.append(line)
+    return matrix
 
 
-def macierz_sasiedztwa(graph):
+def macierz_sasiedztwa(matrix):
     print("\nMacierz sasiedztwa")
-    pass
-    # return matrix
+    headings = [" "] + [f"V{j+1}" for j in range(len(matrix))]
+    i = 1
+    for line in matrix:
+        line.insert(0, f"V{i}")
+        i += 1
+    print(tabulate(matrix, headers=headings, tablefmt='orgtbl'))
 
 
-def lista_nastepnikow(graph):
+def lista_nastepnikow(matrix):
     print("\nLista nastepnikow")
-    pass
-    # return list
+    result = []
+    result2 = []  # jesli by byla do czegos potrzebna
+    n = len(matrix)
+    for i in range(n):
+        nastepniki = []
+        for j in range(n):
+            if matrix[i][j] == 1:
+                nastepniki.append(j+1)
+        result.append([i+1, " -> ".join(list(map(str, nastepniki)))])
+        result2.append([i+1, nastepniki])  # jesli by byla do czegos potrzebna
+    print(tabulate(result, headers=['V', 'lista'], tablefmt='orgtbl'))
 
 
-def tabela_krawedzi(graph):
+def tabela_krawedzi(matrix):
     print("\nTabela krawedzi")
-    pass
-    # return table
+    n = len(matrix)
+    table = []
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] == 1:
+                table.append([i+1, j+1])
+    print(tabulate(table, headers=["out", "in"], tablefmt='orgtbl'))
 
 
 def przegladanieDFS(graph):
@@ -48,9 +76,17 @@ def sortowanieBFS(graph):
     pass
 
 
+example = [[0, 1, -1, 0, -1],
+           [-1, 0, -1, 1, 1],
+           [1, 1, 0, -1, 0],
+           [0, -1, 1, 0, -1],
+           [1, -1, 0, 1, 0]]
+
 while True:
     try:
-        x = int(input("\nWybierz opcje:\n0.Wyjscie\n1.Wygenerowac graf losowo\n2.Podac macierz sasiedztwa z klawiatury\n->"))
+        x = int(input("\nWybierz opcje:\n0.Wyjscie\n"
+                      "1.Wygenerowac graf losowo\n"
+                      "2.Podac macierz sasiedztwa z klawiatury\n->"))
     except ValueError:
         print("Nalezy podac liczbe 0-2.")
         continue
@@ -60,17 +96,19 @@ while True:
         # graph = generate()
         generate()
     elif x == 2:
-        # graph = generate()
-        create_graph()
+        matrix = create_graph()
     else:
         print("Nalezy podac liczbe 0-2.")
         continue
-    # print(macierz_sasiedztwa(graph))
-    # print(lista_nastepnikow(graph))
-    # print(tabela_krawedzi(graph))
+    macierz_sasiedztwa(deepcopy(matrix))
+    lista_nastepnikow(deepcopy(matrix))
+    tabela_krawedzi(deepcopy(matrix))
     while True:
         try:
-            n = int(input("\nWybierz dzialanie dla podanego grafu:\n0.Wyjscie\n1.Przegladanie DFS\n2.Przegladanie BFS\n3.Sortowanie topologiczne DFS\n"
+            n = int(input("\nWybierz dzialanie dla podanego grafu:\n0.Wyjscie\n"
+                          "1.Przegladanie DFS\n"
+                          "2.Przegladanie BFS\n"
+                          "3.Sortowanie topologiczne DFS\n "
                           "4.Sortowanie topologiczne BFS\n->"))
         except ValueError:
             print("Nalezy podac liczbe 0-4.")
