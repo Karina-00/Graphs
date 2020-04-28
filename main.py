795c0beeb575e1dec4ddd15469cf9185e6ee0193
Karina-00 <karina1szubert@gmail.com>
2020-04-29 01:11:45 +0200

    dfs sort

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tabulate import tabulate
 from click import style
 
 from ggen import Generator
-from repr import *
+# from repr import *
 
 
 def create_graph():
@@ -19,7 +19,7 @@ def create_graph():
 
 
 def macierz_sasiedztwa(matrix):
-    print("\nMacierz sasiedztwa")
+    print(style("\nMacierz sasiedztwa", fg='blue'))
     headings = [" "] + [f"V{j+1}" for j in range(len(matrix))]
     i = 1
     for line in matrix:
@@ -29,7 +29,7 @@ def macierz_sasiedztwa(matrix):
 
 
 def lista_nastepnikow(matrix):
-    print("\nLista nastepnikow")
+    print(style("\nLista nastepnikow", fg='blue'))
     result = []
     result2 = []
     n = len(matrix)
@@ -45,7 +45,7 @@ def lista_nastepnikow(matrix):
 
 
 def tabela_krawedzi(matrix):
-    print("\nTabela krawedzi")
+    print(style("\nTabela krawedzi", fg='blue'))
     n = len(matrix)
     table = []
     for i in range(n):
@@ -70,7 +70,7 @@ def dfs(v, result, successors):
 
 
 def przegladanie_dfs(successors):
-    print("\nPrzegladanie DFS")
+    print(style("\nPrzegladanie DFS", fg='blue'))
     res = []
     for i in range(len(successors)):
         res = dfs(successors[i][0], res, successors)
@@ -96,7 +96,7 @@ def bfs(v, result, successors):
 
 
 def przegladanie_bfs(successors):
-    print("\nPrzegladanie BFS")
+    print(style("\nPrzegladanie BFS", fg='blue'))
     res = []
     for i in range(len(successors)):
         nastepniki = successors[i][0]
@@ -107,40 +107,8 @@ def przegladanie_bfs(successors):
     return res
 
 
-def dfs_sort_matrix(matrix):
-    res = []
-    print(style("Sortowanie DFS >> Macierz", fg='blue'))
-    # for i in range(len(matrix)):
-    #     if matrix[i][i] == 0:
-    #         matrix[i][i] = 1  # "szary"
-    #     if
-
-
-# FUNCS
-def next_by_matrix(matrix, i):
-    successors = []
-    for j in range(len(matrix[i])):
-        if matrix[i][j] == 1:
-            successors.append(j + 1)
-    return successors
-
-
-def next_by_list(list, i):
-    return list[i][1]
-
-
-def next_by_table(table, i):
-    successors = []
-    for j in range(len(table)):
-        if table[j][0] == i + 1:
-            successors.append(table[j][1])
-    return successors
-
-
 # sortowanie BFS
-# macierz
-
-
+# matrix
 def create_in_degree_matrix(matrix):
     in_degree = []
     for i in range(len(matrix)):
@@ -149,13 +117,22 @@ def create_in_degree_matrix(matrix):
     return in_degree
 
 
+def next_by_matrix(matrix, i):
+    successors = []
+    for j in range(len(matrix[i])):
+        if matrix[i][j] == 1:
+            successors.append(j + 1)
+    return successors
+
+
 def sort_bfs_by_matrix(matrix):
     in_degree = create_in_degree_matrix(matrix)
-    print("\nDla macierzy sasiedztwa")
+    print(style("\nSortowanie BFS >> Macierz sasiedztwa", fg='blue'))
     return sortowanieBFS(in_degree, next_by_matrix, matrix)
+# matrix end ---
 
 
-# lista
+# list
 def create_in_degree_list(list, n):
     in_degree = []
     flatten = sum([list[k][1] for k in range(len(list))], [])
@@ -171,11 +148,12 @@ def next_by_list(list, i):
 
 def sort_bfs_by_list(list, n):
     in_degree = create_in_degree_list(list, n)
-    print("\nDla listy nastepnikow")
+    print(style("\nSortowanie BFS >> Lista nastepnikow", fg='blue'))
     return sortowanieBFS(in_degree, next_by_list, list)
+# list end ---
 
 
-# tabela
+# table
 def create_in_degree_table(table, n):
     in_degree = []
     flatten = [table[k][1] for k in range(len(table))]
@@ -185,12 +163,22 @@ def create_in_degree_table(table, n):
     return in_degree
 
 
+def next_by_table(table, i):
+    successors = []
+    for j in range(len(table)):
+        if table[j][0] == i + 1:
+            successors.append(table[j][1])
+    return successors
+
+
 def sort_bfs_by_table(table, n):
-    print("\nDla tabeli krawedzi")
+    print(style("\nSortowanie BFS >> Tabela krawedzi", fg='blue'))
     in_degree = create_in_degree_table(table, n)
     return sortowanieBFS(in_degree, next_by_table, table)
+# table end ---
 
 
+# main bfs_sort
 def lower_degree(successors, in_degree):
     for i in range(len(in_degree)):
         if in_degree[i][0] in successors:
@@ -208,6 +196,45 @@ def sortowanieBFS(in_degree, find_next, data):
                 successors = find_next(data, i)
                 in_degree = lower_degree(successors, in_degree)
     return " -> ".join(list(map(str, result)))
+
+
+# sortowanie DFS -------------
+def dfs_sort_matrix(matrix):
+    print(style("\nSortowanie DFS >> Macierz sasiedztwa", fg='blue'))
+    return sortowanieDFS(next_by_matrix, matrix, len(matrix))
+
+
+def dfs_sort_list(list, n):
+    print(style("\nSortowanie DFS >> Lista nastepnikow", fg='blue'))
+    return sortowanieDFS(next_by_list, list, n)
+
+
+def dfs_sort_table(table, n):
+    print(style("\nSortowanie DFS >> Tabela krawedzi", fg='blue'))
+    return sortowanieDFS(next_by_table, table, n)
+
+
+def dfs_sort(v, szare, result, find_next, data):
+    if v in result:
+        return result, szare
+    szare.append(v)
+    successors = find_next(data, v-1)
+    for el in successors:
+        result, szare = dfs_sort(el, szare, result, find_next, data)
+        if el not in result:
+            result.append(el)
+    return result, szare
+
+
+def sortowanieDFS(find_next, data, nr_of_vertexes):
+    result = []
+    szare = []
+    n = nr_of_vertexes
+    for v in range(1, n+1):
+        if v not in result:
+            result, szare = dfs_sort(v, szare, result, find_next, data)
+            result.append(v)
+    return " -> ".join(list(map(str, result[::-1])))
 
 
 if __name__ == '__main__':
@@ -255,6 +282,8 @@ if __name__ == '__main__':
             elif n == 3:
                 print("\n-----Sortowanie topologiczne DFS-----")
                 print(dfs_sort_matrix(deepcopy(matrix)))
+                print(dfs_sort_list(successors_list, len(matrix)))
+                print(dfs_sort_table(edge_table, len(matrix)))
             elif n == 4:
                 print("\n-----Sortowanie topologiczne BFS-----")
                 print(sort_bfs_by_matrix(deepcopy(matrix)))
